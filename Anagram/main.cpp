5c423f2319425a4af3812b3c78e76db6fd4eff19
WonJin Kim <wonjin.kim@mail.utoronto.ca>
2019-06-27 20:03:01 +0900

    file upload

---
+++ Anagram/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <algorithm>
+
+int a[60], b[60];
+int main(){
+    freopen("input.txt", "rt", stdin);
+    char str[100];
+    scanf("%s", &str);
+    for(int i=0; str[i]!='\0'; i++){
+        if(str[i]>=65 && str[i]<=90){
+            a[str[i]-64]++;
+        }
+        else a[str[i]-70]++;
+    }
+    scanf("%s", &str);
+    for(int i=0; str[i]!='\0'; i++){
+        if(str[i]>=65 && str[i]<=90){
+            b[str[i]-64]++;
+        }
+        else b[str[i]-70]++;
+    }
+    for(int i=1; i<=52; i++){
+        if(a[i]!=b[i]){
+            printf("NO\n");
+            exit(0);
+        }
+    }
+    printf("YES\n");
+    return 0;
+    
+}
+
